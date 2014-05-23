67d9c6b6e95178f81c0705180033a33d4c69ecb6
Gabriel Schade Cardoso <gabriel.schade@hotmail.com>
2014-05-23 02:58:07 -0300

    Cap7- CompreAquiParteII

---
+++ CSharp/Capitulo7/CompreAqui - Parte II/CompreAqui/Paginas/SuaConta.xaml.cs
@@ -0,0 +1,112 @@
+﻿using CompreAqui.Modelos;
+using CompreAqui.Resources;
+using CompreAqui.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkID=390556
+
+namespace CompreAqui.Paginas
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class SuaConta : Page
+    {
+        private UsuarioVM _usuarioVM;
+        private Usuario _usuarioAtual;
+
+        public SuaConta()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.
+        /// This parameter is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            if (_usuarioVM == null)
+                _usuarioVM = new UsuarioVM();
+            this.DataContext = _usuarioVM;
+
+            Windows.Storage.ApplicationDataContainer configuracoes =
+                Windows.Storage.ApplicationData.Current.LocalSettings;
+
+            _usuarioAtual = BancoDados.Instancia
+                                  .Usuarios
+                                  .FirstOrDefault
+                                  (usuario => usuario.Id == Convert.ToInt32(configuracoes.Values["usuarioId"]));
+            if (_usuarioAtual != null)
+            {
+                _usuarioVM.Nome = _usuarioAtual.NomeUsuario;
+                _usuarioVM.Email = _usuarioAtual.Email;
+            }
+        }
+
+        private async void Button_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            string validacoes = _usuarioVM.ValidarCamposTrocaSenha();
+            if (string.IsNullOrEmpty(validacoes))
+            {
+                if (_usuarioAtual.Senha == _usuarioVM.SenhaAntesAlteracao)
+                {
+                    _usuarioAtual.Senha = _usuarioVM.Senha;
+                    await _usuarioAtual.AtualizarDados();
+
+                    await ExibirMensagem("Sua senha foi atualizada com sucesso!");
+                    _usuarioVM.SenhaAntesAlteracao = string.Empty;
+                    _usuarioVM.Senha = string.Empty;
+                    _usuarioVM.ConfirmacaoSenha = string.Empty;
+                }
+                else
+                    await ExibirMensagem("- O campo Senha atual não está com a senha correta");
+            }
+            else
+            {
+                await ExibirMensagem(validacoes);
+            }
+        }
+
+        private async Task ExibirMensagem(string mensagem)
+        {
+            MessageDialog messageDialog = new MessageDialog(mensagem);
+            await messageDialog.ShowAsync();
+        }
+
+        private async void AppBarButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageDialog mensagem = new MessageDialog("Tem certeza que deseja desconectar sua conta do aplicativo?", "Confirmação");
+            mensagem.Commands.Add(new UICommand("Sim", delegate(IUICommand command)
+            {
+                Windows.Storage.ApplicationDataContainer configuracoes =
+            Windows.Storage.ApplicationData.Current.LocalSettings;
+
+                configuracoes.Values["usuarioId"] = 0;
+                this.Frame.Navigate(typeof(MainPage));
+                this.Frame.BackStack.Clear();
+            }));
+
+            mensagem.Commands.Add(new UICommand("Não"));
+
+            await mensagem.ShowAsync();
+        }
+
+    }
+}
---
+++ CSharp/Capitulo7/CompreAqui - Parte II/CompreAqui/Resources/BancoDados.cs
@@ -0,0 +1,94 @@
+﻿using CompreAqui.Modelos;
+using CompreAqui.ViewModels;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace CompreAqui.Resources
+{
+    public class BancoDados
+    {
+        private const string DATA_FILE = "data.JSON";
+
+        private BancoDados()
+        { }
+
+        private static BancoDados instancia;
+        public static BancoDados Instancia
+        {
+            get
+            {
+                return CriarOuObterInstancia();
+            }
+            set
+            {
+                instancia = value;
+            }
+        }
+
+        public static BancoDados CriarOuObterInstancia()
+        {
+            if (instancia == null)
+            {
+                instancia = new BancoDados();
+                instancia.Usuarios = new List<Usuario>();
+            }
+
+            return instancia;
+        }
+
+        
+
+        public async Task BuscarDados()
+        {
+            StorageFolder pasta = ApplicationData.Current.LocalFolder;
+            StorageFile arquivo = await pasta.CreateFileAsync(DATA_FILE, CreationCollisionOption.OpenIfExists);
+            string resultado = await FileIO.ReadTextAsync(arquivo);
+            if (string.IsNullOrEmpty(resultado))
+            {
+                Usuario admin = new Usuario();
+                admin.NomeUsuario = "admin";
+                admin.Senha = "admin";
+                admin.Email = "admin@compreaqui.com.br";
+                admin.Id = 1;
+
+                Usuarios.Add(admin);
+                await GravarDados();
+                await BuscarDados();
+            }
+            else
+                Usuarios = JsonConvert.DeserializeObject<List<Usuario>>(resultado);
+        }
+
+        public async Task GravarDados()
+        {
+            StorageFolder pasta = ApplicationData.Current.LocalFolder;
+            StorageFile arquivo = await pasta.CreateFileAsync(DATA_FILE, CreationCollisionOption.OpenIfExists);
+            await FileIO.WriteTextAsync(arquivo, JsonConvert.SerializeObject(Usuarios), Windows.Storage.Streams.UnicodeEncoding.Utf8);
+        }
+
+        public async void AdicionarUsuario(UsuarioVM usuarioVM, bool Autenticar = false)
+        {
+            Usuario novoUsuario = new Usuario();
+            novoUsuario.Email = usuarioVM.Email;
+            novoUsuario.NomeUsuario = usuarioVM.Nome;
+            novoUsuario.Senha = usuarioVM.Senha;
+            novoUsuario.EntrarAutomaticamente = usuarioVM.EntrarAutomaticamente;
+
+
+            novoUsuario.Id = Usuarios.Count + 1;
+            Usuarios.Add(novoUsuario);
+            await GravarDados();
+
+            if (Autenticar)
+                novoUsuario.Autenticar();
+        }
+
+        public List<Usuario> Usuarios { get; private set; }
+    }
+}
